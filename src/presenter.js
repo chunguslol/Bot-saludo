14fa75f5c219486818e880e05975bea69445d338
Agustin Ledezma <agustin.ledezma@ucb.edu.bo>
2024-02-15 22:33:35 -0400

    saludar tomando encuenta la hora

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,14 @@
 
 import saludar from "./saludar";
 
+
+
+
 const nombre_input = document.querySelector("#nombre");
 const edad_input = document.querySelector("#edad");
 const genero_select = document.querySelector("#genero");
+const hora_input = document.querySelector("#hora");
+const idioma_select = document.querySelector("#idioma");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -12,6 +17,10 @@ form.addEventListener("submit", (event) => {
   const nombre = nombre_input.value;
   const edad = parseInt(edad_input.value);
   const genero = genero_select.value;
+  const horaParts = hora_input.value.split(':');
+  const hora = new Date();
+  hora.setHours(parseInt(horaParts[0], 10));
+  hora.setMinutes(parseInt(horaParts[1], 10));
 
   let saludo = "Hola";
 
@@ -23,13 +32,19 @@ form.addEventListener("submit", (event) => {
     if (genero === "femenino") {
       saludo = "Hola Sra. " + nombre;
     } else {
-      saludo = "Hola Sr. " + nombre;
+      saludo = "Hola Sr " + nombre;
     }
   } else if (edad <= 30) {
     saludo = " Hola Joven " + nombre;
   }
 
+  if (hora.getHours() < 12) {
+    saludo += ", Buenos días";
+  } else if (hora.getHours() < 18) {
+    saludo += ", Buenas tardes";
+  } else {
+    saludo += ", Buenas noches";
+  }
 
   div.innerHTML = "<p>" + saludo + "</p>";
-
 });
