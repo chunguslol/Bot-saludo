396102e599d10d360f32d1c83c4b06c4b2909f78
Agustin Ledezma <agustin.ledezma@ucb.edu.bo>
2024-02-15 22:27:51 -0400

    Salidar segun el genero y la edad ya que estan ligados

--- src/presenter.js
+++ src/presenter.js
@@ -2,12 +2,16 @@
 import saludar from "./saludar";
 
 const nombre_input = document.querySelector("#nombre");
+const edad_input = document.querySelector("#edad");
+const genero_select = document.querySelector("#genero");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const nombre = nombre_input.value;
+  const edad = parseInt(edad_input.value);
+  const genero = genero_select.value;
 
   let saludo = "Hola";
 
@@ -15,5 +19,17 @@ form.addEventListener("submit", (event) => {
     saludo += " " + nombre;
   }
 
+  if (edad > 30) {
+    if (genero === "femenino") {
+      saludo = "Hola Sra. " + nombre;
+    } else {
+      saludo = "Hola Sr. " + nombre;
+    }
+  } else if (edad <= 30) {
+    saludo = " Hola Joven " + nombre;
+  }
+
+
   div.innerHTML = "<p>" + saludo + "</p>";
+
 });
